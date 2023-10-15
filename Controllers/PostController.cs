5f24110da498625c77499c56fc319ee3098dc6f3
Puzzle-Queen <81598037+SAttakai13@users.noreply.github.com>
2023-10-15 10:18:47 -0600

    Basic Controller and View Set ups, common decorations

---
+++ Controllers/PostController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Artbase.Controllers
+{
+    public class PostController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
