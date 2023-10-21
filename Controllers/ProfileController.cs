8c9b65839f82bb61550acf5cfffba6637bc5e20b
Puzzle-Queen <81598037+SAttakai13@users.noreply.github.com>
2023-10-20 23:09:27 -0600

    Changed some of the controllers and added in some basic connections

--- Controllers/ProfileController.cs
+++ Controllers/ProfileController.cs
@@ -1,9 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Artbase.Data;
+using Artbase.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Artbase.Controllers
 {
     public class ProfileController : Controller
     {
+        private readonly UserProfileDAL _profileService;
+        
+        public ProfileController(UserProfileDAL profileService)
+        {
+            _profileService = profileService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AllProfiles()
+        {
+            return View(await _profileService.GetProfile());
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> OtherUserProfilePage(string? userId)
+        {
+            var profile = await _profileService.GetProfileByUserId(userId);
+            
+            if (profile is null)
+            {
+                return NotFound();
+            }
+            return View(profile);
+        }
+
+
         public IActionResult Index()
         {
             return View();
@@ -13,6 +41,22 @@ namespace Artbase.Controllers
         {
             return View();
         }
+
+        [HttpGet]
+        public IActionResult AddProfile()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddProfile(Profile prof)
+        {
+            if (ModelState.IsValid)
+            {
+                
+            }
+        }
+
 
     }
 }
