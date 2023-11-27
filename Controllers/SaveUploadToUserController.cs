48f7fe0fccb1e363617f54b044819baf88561bd5
Puzzle-Queen <81598037+SAttakai13@users.noreply.github.com>
2023-11-26 19:24:48 -0700

    The user uploads can be seen better now, the carousel is still wonky. You can delete uploads.

---
+++ Controllers/SaveUploadToUserController.cs
@@ -0,0 +1,38 @@
+﻿using Artbase.Data;
+using Artbase.Interfaces;
+using Artbase.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Artbase.Controllers
+{
+    public class SaveUploadToUserController : Controller
+    {
+        ISaveUploadToUser saveUp;
+        public SaveUploadToUserController(ISaveUploadToUser saveUp)
+        {
+            this.saveUp = saveUp;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SavePost(int? id)
+        {
+            string currentUser = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            SaveUploadToUser usersave = new SaveUploadToUser(id, currentUser);
+            if(ModelState.IsValid)
+            {
+                saveUp.SaveUpload(usersave);
+                return RedirectToAction("UserProfilePage", "Profile");
+            }
+            return View("Index", "Home");
+        }
+
+
+
+    }
+}
